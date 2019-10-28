e0cf44436d87db2cdc5d6d18d7fe2262758b6910
Una Kamilla <unakamilla@gmail.com>
2019-10-28 18:47:32 +0000

    byrjun lol

--- script.js
+++ script.js
@@ -23,30 +23,37 @@
   * Ef notandi ýtir á "cancel" þá er sótt niðurstöður með getResults() og þær birtar með alert().
   */
 function start() {
-  play();
+  alert("Þessi leikur gengur út á að giska á tölu milli 1 og 100 í nokkrum tilraunum");
+  do {
+    play();
+  }    while(confirm("Viltu spila annan leik?"))
+
 }
 
 /**
  * Spilar einn leik. Sér um að:
  *  - Velja tölu af handahófi í byrjun með randomNumber()
  *  - Biðja notanda um tölu með prompt()
- *  - Vinna úr intaki frá notanda með parseGuess()
+ *  - Vinna úr inntaki frá notanda með parseGuess()
  *  - Láta vita hversu nálægt eða rétt gisk er með getResponse() og alert()
  *  - Haldautan um fjölda ágiskana
  *  - Vista fjölda ágiskana í "games" fylki þegar búið er að giska rétt
- * 
+ *
  * Ef notandi ýtir á cancel þegar beðið er um ágiskun skal hætta í leik en ekki vista ágiskanir
  *  - t.d. með því að nota break í lykkju.
- * 
- * Þarf aðútfæra með lykkju og flæðisstýringum
+ *
+ * Þarf að útfæra með lykkju og flæðisstýringum
  */
 function play() {
-  const random = randomNumber(1,100;
+  const random = randomNumber(1,100);
+  input = prompt("Veldu tölu milli 1 og 100");
+  inputParsed = parseInt(input)
+
 }
 
 /**
  * Skilar niðurstöðum um spilaða leiki sem streng.
- * Fjöldi liekja er skilað ásamt meðalfjölda giska, t.d.:
+ * Fjöldi leikja er skilað ásamt meðalfjölda giska, t.d.:
  *    "þú spilaðir 10 leiki
  *     Meðalfjöldi ágiskana var 5"
  * ATH að meðalfjöldi kemur í nýrri línu.
@@ -58,11 +65,11 @@ function getResults(){
 }
 
 /**
- * Reiknar út og skilar meðal ágiskunum í öllum leikjum sem geymdir eru í 
+ * Reiknar út og skilar meðal ágiskunum í öllum leikjum sem geymdir eru í
  * global breytu "games". Skilar gildi með tveim aukastöfum.
  * Ef games = [3,3,4] er niðurstaðan (3+3+4)/3 = 3.66666667
  * og henni skilað sem 3.67
- * 
+ *
  * þarf að útfæra með lykkju.
  */
 function calculateAverage(){
@@ -88,7 +95,7 @@ function parseGuess(input){
  * Ef munur er undir 20 skal skila "Frekar langt frá"
  * Ef munur er undir 50 skal skila "Langt frá"
  * Annars skal skila "Mjög langt frá"
- * 
+ *
  * Þarf að útfæra með flæðistýringu.
  * Math.abs skilar algildi tölu: |a| = Math.abs(a)
  */
